07e44fbc62af30b7e4f9270bebe8226071acf491
mouafak-khaled <mhasan18@ku.edu.tr>
2021-04-26 16:00:46 +0300

    create regression test class for testing

--- Homework2/RegressionTest.py
+++ Homework2/RegressionTest.py
@@ -6,21 +6,15 @@ import unittest
 class RegressionTest(unittest.TestCase):
 
     def empty_test(self):
-        self.assertEqual('Fizz', fizzbuzz.FizzBuzz(9))
-        self.assertNotEqual('Fizz', fizzbuzz.FizzBuzz(15))
-
+        
     def correct_test(self):
-        self.assertEqual('Buzz', fizzbuzz.FizzBuzz(10))
 
     def data_shape_test(self):
-        self.assertEqual('FizzBuzz', fizzbuzz.FizzBuzz(15))
 
-    def test_error(self):
-        with self.assertRaises(TypeError):
-            fizzbuzz.FizzBuzz('b')
+    def parameter_type_test(self):
+
 
     def test5(self):
-        self.assertEqual('Buzz', fizzbuzz.FizzBuzz(15))
 
 
 if __name__ == '__main__':  # Add this if you want to run the test with this script.
