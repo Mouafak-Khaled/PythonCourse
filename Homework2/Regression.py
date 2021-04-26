3ec7c3dc6617c6814221aa3d4d2f8ca334a184cd
Mouafak Alsaid Hasan <=>
2021-04-26 15:31:03 +0300

    implement the regression method with list wise deletion

--- Homework2/Regression.py
+++ Homework2/Regression.py
@@ -2,23 +2,24 @@ import numpy as np
 import pandas as pd
 
 
-def linearRegresson(X, Y):
+def linearRegression(X, Y):
 
-    if X.size == 0 or Y.size = 0:
-        return (-1, -1, -1, -1)
 
-    if not isinstance(X, np.array):
-        print("X must be a numpy array!")
-        return (-1, -1, -1, -1)
+    if not isinstance(X, (np.ndarray, list)):
+        raise Exception("X must be a numpy array or a normal python array!")
+        return -1, -1, -1, -1
 
-    if not isinstance(Y, np.array):
-        print("Y must be a numpy array!")
-        return (-1, -1, -1, -1)
+    if not isinstance(Y,  (np.ndarray, list)):
+        raise Exception("Y must be a numpy array or a normal python array!")
+        return -1, -1, -1, -1
 
-    if (X.shape != Y.shape):
-        print("X and Y must have equal shape!")
-        return (-1, -1, -1, -1)
+    if X.shape != Y.shape:
+        raise Exception("X and Y must have equal shape!")
+        return -1, -1, -1, -1
 
+    #handling the Nan values in my data
+    list_wise_delete(X, Y)
+    
     N = X.shape[0]
     K = X.shape[1]
 
@@ -30,14 +31,34 @@ def linearRegresson(X, Y):
 
     #Calculating the estimate
     X_transpose_inverse = np.linalg.inv(np.matmul(X.transpose(), X))
+    print("(X'X) inverse Shape", X_transpose_inverse.shape)
     beta_estimate = np.matmul(np.matmul(X_transpose_inverse, X.transpose()), Y)
 
-    error = Y - np.matmul(new_X, beta_estimate)
+    #calculating the errors
+    error = Y - np.matmul(X, beta_estimate)
     error = np.array(error)
 
+    #calculating the variances
     segma_square = np.matmul(error.transpose(), error)/(N - K - 1)
-    variance = np.matmul(segma_square, X_transpose_inverse)
+    print("segma square shape: ", segma_square.shape)
+    Var = np.multiply(segma_square, X_transpose_inverse)
 
+    #calculating the credible intervals
+
+    STD_ERROR = np.sqrt(Var.diagonal() / N)
+
+    CL = beta_estimate - 1.96 * STD_ERROR
+    CR = beta_estimate + 1.96 * STD_ERROR
+
+    return beta_estimate, STD_ERROR, CL, CR
+
+
+def list_wise_delete(X, Y):
+
+    for i in range(len(X)):
+        if not isinstance(X[i], (float, int)) or not isinstance(Y[i], (float, int)):
+            np.delete(X, X[i])
+            np.delete(Y, Y[i])
 
 
 
